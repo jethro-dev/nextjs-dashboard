31121afada73f20c4acfa1925ba1586fa535c599
Jethro <jethro@livr.co>
2023-11-20 13:47:16 +0000

    initial commit

---
+++ app/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const CustomersPage = (props: Props) => {
+  return <p>Customers Page</p>;
+};
+
+export default CustomersPage;
---
+++ app/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const InvoicesPage = (props: Props) => {
+  return <p>Invoices Page</p>;
+};
+
+export default InvoicesPage;
